0124b79170421701707e28f5edeaabdb7900e377
Nikhil Verma <nikhil.verma@twinspark.co>
2020-05-11 18:59:15 +0530

    remove link

--- wp-content/themes/bluecorona-child-theme/inc/widgets/bc-footer-address-widget.php
+++ wp-content/themes/bluecorona-child-theme/inc/widgets/bc-footer-address-widget.php
@@ -28,7 +28,7 @@ class BC_Footer_Address_Widget extends WP_Widget {
     </div>
     <div class="media mx-auto border_bottom_primary_3 mobile-text-align">
         <i class="align-self-center far fa-clock bc_text_36 bc_line_height_22 bc_color_primary"></i>
-        <a href="javascript:vodi(0);" class="no_hover_underline">
+        <a class="no_hover_underline">
             <div class="media modal-body">
                 <span class="bc_text_20 bc_sm_text_16 bc_sm_line_height_26 bc_line_height_26 bc_color_quaternary bc_text_normal ">1Monday - Friday <br> 7:30am - 5pm</span>
                 </span>
