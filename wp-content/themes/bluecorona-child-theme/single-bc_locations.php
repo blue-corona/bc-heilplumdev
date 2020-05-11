f005f16d5b5fa82191acf7371b7b67b4b06f7344
Nikhil Verma <nikhil.verma@twinspark.co>
2020-05-11 18:05:20 +0530

    update titles

--- wp-content/themes/bluecorona-child-theme/single-bc_locations.php
+++ wp-content/themes/bluecorona-child-theme/single-bc_locations.php
@@ -25,7 +25,7 @@ global $post;
 		<div class="container">
 		    <div class="row">
 			    <div class="col-lg-8 col-md-12 col-xs-12 mt-5 px-5 sub_page_sidebar">
-                    <h2><?php the_title();?></h2>
+                    <h1><?php the_title();?></h1>
 					<?php 
                     if ( have_posts() ) : 
                         while ( have_posts() ) : the_post();
