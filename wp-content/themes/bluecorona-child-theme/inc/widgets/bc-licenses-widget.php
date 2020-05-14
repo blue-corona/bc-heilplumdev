e6018df65566204d66f6fa16db406de07a0dba1e
Humayun Khan <humayun.khan@twinspark.co>
2020-05-14 15:11:14 +0530

    completed #20,#36,#38

--- wp-content/themes/bluecorona-child-theme/inc/widgets/bc-licenses-widget.php
+++ wp-content/themes/bluecorona-child-theme/inc/widgets/bc-licenses-widget.php
@@ -17,7 +17,7 @@ class BC_Licenses_Widget extends WP_Widget {
         echo $args['before_widget'];
         ?>
         <span class="bc_text_36 bc_line_height_40 bc_sm_text_26 bc_color_10 bc_font_alt_1 bc_text_semibold">Licenses</span>
-        <span class="bc_text_20 bc_sm_text_16 bc_sm_line_height_26 bc_line_height_30 bc_color_quaternary bc_text_normal d-block">MD #6428<br class="d-none d-lg-block">
+        <span class="bc_text_20 bc_sm_text_16 bc_sm_line_height_26 bc_line_height_30 bc_color_quaternary bc_text_normal d-block mt-4">MD #6428<br class="d-none d-lg-block">
         WSSC #243<br class="d-none d-lg-block">
         BALT CO. #910<br class="d-none d-lg-block">
         AA #A2 00936<br class="d-none d-lg-block">
