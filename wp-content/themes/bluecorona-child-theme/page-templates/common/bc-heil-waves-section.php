ead8c15fd82c01eefc574e705bff292f00cdc09e
rohan raj mishra <rohan@twinspark.co>
2020-05-14 14:34:06 +0530

    inv subpage 22

--- wp-content/themes/bluecorona-child-theme/page-templates/common/bc-heil-waves-section.php
+++ wp-content/themes/bluecorona-child-theme/page-templates/common/bc-heil-waves-section.php
@@ -1,5 +1,5 @@
-<div class="container-fluid" style="height:170px; background-position: center; background-size: cover; background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/img/waves.png">
-    <div class="container">
+<div class="container-fluid p-0 position-relative" style="height:170px; background-position: center; background-size: contain; background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/img/waves.png')">
+    <div class="container-fluid position-absolute p-0" style="height:1px; background-color: #EBEBEB; bottom:0px;">
         <div class="row"></div>
     </div>
 </div>
