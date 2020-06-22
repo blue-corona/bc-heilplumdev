6c76eb0f7dfb2cc559224e0149229f43389d024d
jameskukreja-ts <james.kukreja@twinspark.co>
2020-06-22 14:31:00 +0530

    Update bc-hero-banner.php

--- wp-content/themes/bluecorona-child-theme/page-templates/hero-section/bc-hero-banner.php
+++ wp-content/themes/bluecorona-child-theme/page-templates/hero-section/bc-hero-banner.php
@@ -52,7 +52,7 @@
                         </div>
                         <div class="row">
                            <div class="col-12 px-5">
-                                <span class="bc_text_28 bc_line_height_27 bc_font_alt_1 bc_text_bold bc_color_3 d-block mt-3 bc_hero_form_title"> How Can We Help?</span>
+                                <span class="bc_text_28 bc_line_height_27 bc_font_alt_1 bc_text_bold bc_color_3 d-block mt-3 bc_hero_form_title_text"> How Can We Help?</span>
                                <?php echo do_shortcode('[gravityform id=1 ajax=true]')?>
                               <!--  <span class="d-block mt-n2 bc_text_12 bc_line_height_36 bc_font_default bc_color_5 bc_text_bold">
                                    <input type="checkbox" class="bc_text_18 bc_line_height_50 bc_color_5" style="height: 13px;">
@@ -69,7 +69,7 @@
 <script>
     jQuery(document).on("gform_confirmation_loaded", function (e, form_id) {
 	if(form_id == 1){
-		jQuery(".bc_hero_form_title").html("Thank you!");
+		jQuery(".bc_hero_form_title_text").html("Thank you!");
 	}
     });
 </script>
